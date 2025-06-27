cd5b36b161564d7799128f3cfde6f00c8327709e
Nipesh <nipeshincbca2021@gmail.com>
2025-06-27 20:26:36 +0545

    edit and delete operation complete

--- my_project/my_app/urls.py
+++ my_project/my_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views.main_view import index,create_employee,edit_employee
+from .views.main_view import index,create_employee,edit_employee,delete_data
 from .views.auth_view import login_page
 
 urlpatterns = [
     path('',index, name='index'),
     path('login/',login_page, name='login'),
     path('create/',create_employee, name="create"),
-    path('edit/',edit_employee, name="edit")
+    path('edit/<int:id>',edit_employee, name="edit"),
+    path('delete/<int:emp_id>',delete_data, name="delete")
 ]
+
--- my_project/my_app/views/main_view.py
+++ my_project/my_app/views/main_view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from ..models import Employee
 
 def index(request):
@@ -28,6 +28,8 @@ def create_employee(request):
         
         if not phone:
             errors['phone'] = "Phone is required"
+        elif len(phone)<7 or len(phone)>15: 
+            errors['phone'] = "Phone num must contains more than 7 and less than 15 digits"
             
         if not errors:
             data = Employee.objects.create(
@@ -40,15 +42,41 @@ def create_employee(request):
             data.save()
             return redirect('index')
         else:
-            return render(request,'main/CreateEmployee.html',{'error':errors})
+            return render(request,'main/CreateEmployee.html',{'error':errors,'data':request.POST})
    
     return render(request,'main/CreateEmployee.html')
 
-def edit_employee(request):
-    return render(request,'main/EditEmployee.html')
+def edit_employee(request, id):
+    # emp_data = get_object_or_404(Employee, id)
+    emp_data = Employee.objects.get(id=id)
+    if request.method == 'POST':
+        emp_data.fullname = request.POST.get('fullname')
+        emp_data.email = request.POST.get('email')
+        emp_data.salary = request.POST.get('salary')
+        emp_data.address = request.POST.get('address')
+        emp_data.phone = request.POST.get('phone')
+        
+        emp_data.save()
+        return redirect("index")
+    
+    return render(request,'main/EditEmployee.html',{'emp_data':emp_data})
 
  # name = {'key':'value'}
  # arr = [1,2,4,5,6,87,9]
  # fruits = {'apple':200, 'orange':400}
  
  # errors = {'fullname':"Fullname is required",'email':'Email is required'}
+ 
+ 
+ # select * from Employee where id=emp_id   
+ 
+def delete_data(request,emp_id):
+    emp_data = Employee.objects.get(id=emp_id)  # filter()
+    print(emp_data.fullname)
+    emp_data.delete()
+    return redirect('index')
+
+
+    
+ 
+ 
